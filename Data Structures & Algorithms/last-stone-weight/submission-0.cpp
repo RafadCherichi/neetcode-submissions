9eb27232fbccc1d0b3f75604aebc34a140c6971a
Rafad Cherichi <rafad2003@gmail.com>
2026-09-20 00:45:03 +0530

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>maxheap;
+        for(int i =0;i<stones.size();i++){
+            maxheap.push(stones[i]);
+        }
+        int x = 0;
+        int y = 0;
+        while(maxheap.size()>1){
+            y=maxheap.top();
+            maxheap.pop();
+            x=maxheap.top();
+            maxheap.pop();
+            if(x==y){  
+            }
+            if(x<y){
+                maxheap.push(y-x);
+            }
+            if(maxheap.empty()){
+                return 0;
+            }
+            
+        }
+        return maxheap.top();
+    }
+};
